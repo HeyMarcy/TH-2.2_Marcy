b518ff2a8f4ab853da3921d6715af63c74a1bfc2
marcy <marcy.montross@gmail.com>
2017-02-02 14:32:34 -0600

    iteration

---
+++ fizzbuzzJS.js
@@ -0,0 +1,30 @@
+//look for the submit event.
+
+//iterate from 1 to that # 
+
+
+//check to see if that number meets on of three conditions: 
+//
+
+
+
+$(function(){
+
+    $("form").submit(function(e) {e.preventDefault(); 
+    var num = $("#number-choice").val();
+    num = parseInt(num);                          
+    for (var i=0; i<num; i++){ 
+        var result = i;
+        if (i % 3 === 0 && i % 5 === 0){
+            result = "FizzBuzz";  
+        } else if (i % 3 === 0) {
+            result = "Fizz";   
+        } else if (i % 5 === 0){
+            result = "Buzz";
+        }
+        console.log(result);
+    }
+                              
+ });
+
+});
