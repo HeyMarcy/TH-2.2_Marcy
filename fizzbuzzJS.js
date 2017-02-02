54ca6aa2aceb272d2eb1a40685acab276d9f572c
marcy <marcy.montross@gmail.com>
2017-02-02 15:00:25 -0600

    write to div

--- fizzbuzzJS.js
+++ fizzbuzzJS.js
@@ -4,6 +4,8 @@
 
 
 //check to see if that number meets on of three conditions: 
+//write "results" to div.js-results
+
 //
 
 
@@ -11,20 +13,21 @@
 $(function(){
 
     $("form").submit(function(e) {e.preventDefault(); 
-    var num = $("#number-choice").val();
-    num = parseInt(num);                          
-    for (var i=0; i<num; i++){ 
-        var result = i;
-        if (i % 3 === 0 && i % 5 === 0){
-            result = "FizzBuzz";  
-        } else if (i % 3 === 0) {
-            result = "Fizz";   
-        } else if (i % 5 === 0){
-            result = "Buzz";
+        var num = $("#number-choice").val();
+        num = parseInt(num);                          
+        for (var i=0; i<=num; i++){ 
+            var result = i;
+            var fancyResult = '';
+            if (i % 3 === 0 && i % 5 === 0){
+                result = "FizzBuzz";  
+            } else if (i % 3 === 0) {
+                result = "Fizz";   
+            } else if (i % 5 === 0){
+                result = "Buzz";
+            }
         }
-        console.log(result);
-    }
-                              
+            $(".js-results").append( "<p>" + result + "</p>" );
+                      
  });
 
 });
