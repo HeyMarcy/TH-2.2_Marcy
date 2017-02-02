79ab1a78ce533854930d9b429866911d21deb972
marcy <marcy.montross@gmail.com>
2017-02-02 15:13:33 -0600

    done for now

--- fizzbuzzJS.js
+++ fizzbuzzJS.js
@@ -13,21 +13,21 @@
 $(function(){
 
     $("form").submit(function(e) {e.preventDefault(); 
-        var num = $("#number-choice").val();
-        num = parseInt(num);                          
-        for (var i=0; i<=num; i++){ 
-            var result = i;
-            var fancyResult = '';
-            if (i % 3 === 0 && i % 5 === 0){
-                result = "FizzBuzz";  
-            } else if (i % 3 === 0) {
-                result = "Fizz";   
-            } else if (i % 5 === 0){
-                result = "Buzz";
-            }
+    var num = $("#number-choice").val();
+    num = parseInt(num);                          
+    for (var i=0; i<=num; i++){ 
+        var result = i;
+        if (i % 3 === 0 && i % 5 === 0){
+            result = "FizzBuzz";  
+        } else if (i % 3 === 0) {
+            result = "Fizz";   
+        } else if (i % 5 === 0){
+            result = "Buzz";
         }
-            $(".js-results").append( "<p>" + result + "</p>" );
-                      
+     
+    $(".js-results").append( "<p>" + result + "</p>" );
+    }
+                              
  });
 
 });
